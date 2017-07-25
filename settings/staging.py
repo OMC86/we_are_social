f9e685370aa9e45907ee3e39b5f82f274fa296cb
Chris <christoal@outlook.com>
2017-07-25 20:20:13 +0800

    update settings

--- settings/staging.py
+++ settings/staging.py
@@ -4,13 +4,8 @@ import dj_database_url
 DEBUG = False
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(os.getenv('CLEARDB_DATABASE_URL'))
 }
-
-DATABASES['default'] = dj_database_url.parse("CLEARDB_DATABASE_URL")
 
 
 # Stripe environment variables
